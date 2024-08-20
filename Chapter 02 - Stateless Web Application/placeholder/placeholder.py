2c9ef2e88de4b19bb85698499f9b09b252d8f42f
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-20 17:09:00 -0300

    Chapter 02: implementation of image form

--- Chapter 02 - Stateless Web Application/placeholder/placeholder.py
+++ Chapter 02 - Stateless Web Application/placeholder/placeholder.py
@@ -3,11 +3,14 @@ import os
 
 from django.conf import settings
 
+DEBUG= os.environ.get('DEBUG', 'on') == 'on',
+SECRET_KEY= os.environ.get('SECRET_KEY', os.urandom(32)),
+ALLOWED_HOSTS= os.environ.get('ALLOWED_HOSTS', 'localhost').split(','),
 
 settings.configure(
-    DEBUG= os.environ.get('DEBUG', 'on') == 'on',
-    SECRET_KEY= os.environ.get('SECRET_KEY', os.urandom(32)),
-    ALLOWED_HOSTS= os.environ.get('ALLOWED_HOSTS', 'localhost').split(','),
+    DEBUG= DEBUG,
+    SECRET_KEY= SECRET_KEY,
+    ALLOWED_HOSTS= ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
@@ -16,15 +19,34 @@ settings.configure(
     ),
 )
 
-from django.urls import re_path
-from django.http import HttpResponse
+from django import forms
+from django.conf.urls import url
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=4096)
+    width = forms.IntegerField(min_value=1, max_value=4096)
+
+from django.conf.urls import url
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.wsgi import get_wsgi_application
+
+def placeholder(request, width, height):
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        height = form.cleaned_data['height']
+        width = form.cleaned_data['width']
+        return HttpResponse('OK', status=200)
+    return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
     return HttpResponse('Hello World!')
 
 urlpatterns = (
-    re_path(r'^$', index),
+    url(r'^$', index, name='homepage'),
+    url(r^'image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder')
+
 )
 
 # the Web Server Gateway Interface (WSGI) is the specification for how web servers communicate with application frameworks
