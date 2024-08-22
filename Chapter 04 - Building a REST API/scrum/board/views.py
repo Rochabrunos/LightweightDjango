57d5aeafbf9f99a1cdb0598c779470dff65346a7
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-22 16:42:02 -0300

    Chapter 04: sprint, user and task endpoint

--- Chapter 04 - Building a REST API/scrum/board/views.py
+++ Chapter 04 - Building a REST API/scrum/board/views.py
@@ -1,14 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import authentication, permissions, viewsets
 
-from .models import Sprint
-from .serializers import SprintSerializer
-
-# ModelViewSet provides the scaffolding needed for CRUD operations
-class SprintViewSet(viewsets.ModelViewSet):
-    """API endpoint for listing and creating sprints."""
-
-    queryset = Sprint.objects.order_by('end')
-    serializer_class = SprintSerializer
+from .models import Sprint, Task, User
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
 class DefaultMixin(object):
     """Default settings for view authentication, permissions, filtering adn pagination"""
@@ -22,3 +16,25 @@ class DefaultMixin(object):
     paginated_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
+
+# ModelViewSet provides the scaffolding needed for CRUD operations
+class SprintViewSet(viewsets.ModelViewSet):
+    """API endpoint for listing and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
+class TaskView(DefaultMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# ReadOnlyModelViewSet only exposes the action of lookup for a determined User
+class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
+    """API endpoint for listing users."""
+    # Changes the lookup from using the ID of the user to the username
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
