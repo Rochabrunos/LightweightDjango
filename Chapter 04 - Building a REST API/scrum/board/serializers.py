584b1bd61da67c38c007ae30b7cdebe8284a7948
Bruno Rocha <bsrocha1005@gmail.com>
2024-08-23 15:17:45 -0300

    Chapter 04: fix linking resources adding links variable to serialization classes

--- Chapter 04 - Building a REST API/scrum/board/serializers.py
+++ Chapter 04 - Building a REST API/scrum/board/serializers.py
@@ -9,11 +9,13 @@ User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
 
+    links = serializers.SerializerMethodField('get_links')
     class Meta:
         model = Sprint
-        fields = ('id', 'name', 'description', 'end', )
+        fields = ('id', 'name', 'description', 'end', 'links')
     def get_links(self, obj):
         request = self.context['request']
+        print(obj)
         return {
             'self': reverse('sprint-detail', kwargs={'pk': obj.pk}, request=request)
         }
@@ -24,10 +26,11 @@ class TaskSerializer(serializers.ModelSerializer):
         required=False,
         queryset=User.objects.all())
     status_display = serializers.SerializerMethodField('get_status_display')
+    links = serializers.SerializerMethodField('get_links')
     class Meta:
         model = Task
         fields = ('id', 'name', 'description', 'sprint', 'status_display',
-                  'order', 'assigned', 'due', 'completed')
+                  'order', 'assigned', 'due', 'completed', 'links')
     def get_status_display(self, obj):
         return obj.get_status_display(),
     def get_links(self, obj):
@@ -39,10 +42,11 @@ class TaskSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField('get_links')
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active')
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links')
 
     def get_links(self, obj):
         request = self.context['request']
